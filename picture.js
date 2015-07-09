dbe4ece53627f5e570f77c8656ae37267e5ae69b
Kevin Nadro <sodapopcuffgames@gmail.com>
2015-07-09 17:49:25 -0500

    Removing old images

--- picture.js
+++ picture.js
@@ -1,8 +1,5 @@
 var pictures = {
-	"chicago1":"chicago_1.jpg",
-	"chicago2":"chicago_2.jpg",
 	"david":"david.png",
-	"sanfran":"sanfran.jpg",
 	"bird":"bird.jpg",
 	"spacex1":"spacex_1.jpg",
 	"spacex2":"spacex_2.jpg",
